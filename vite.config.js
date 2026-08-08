fd0f2c8faa50dab21dc58459d999b53164fa928d
aetherom <299110591+aetherom@users.noreply.github.com>
2026-08-08 06:36:55 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
-// This automatically uses '/' for your Codespace, and '/DigiMosque/' for GitHub Pages
-const base = process.env.CODESPACE_NAME ? '/' : '/DigiMosque/';
-
 export default defineConfig({
-  base: base,
+  base: './',
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      clientPort: 443
+    }
+  },
   plugins: [
     react(),
     VitePWA({
@@ -19,8 +24,8 @@ export default defineConfig({
         theme_color: '#ffffff',
         background_color: '#ffffff',
         display: 'standalone',
-        scope: base,
-        start_url: base,
+        scope: './',
+        start_url: './',
         icons: [
           { src: 'pwa-192x192.png', sizes: '192x192', type: 'image/png' },
           { src: 'pwa-512x512.png', sizes: '512x512', type: 'image/png' },
